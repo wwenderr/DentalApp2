2722953099d40803a1defed985534d8bec126f28
Anastasia <ad4019809@gmail.com>
2020-11-22 19:52:40 +0300

    layout for quiz fragment

---
+++ app/src/main/java/com/example/dentalapp3/QuizFragment.kt
@@ -0,0 +1,12 @@
+package com.example.dentalapp3
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class QuizFragment: Fragment(R.layout.quiz_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/dentalapp3/articles/SecondFragment.kt
+++ app/src/main/java/com/example/dentalapp3/articles/SecondFragment.kt
@@ -3,6 +3,7 @@ package com.example.dentalapp3.articles
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dentalapp3.R
@@ -11,15 +12,25 @@ import java.lang.reflect.Array
 import java.util.*
 import kotlin.collections.ArrayList
 
-class SecondFragment: Fragment(R.layout.second_fragment) {
+class SecondFragment : Fragment(R.layout.second_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val exampleList = generateDummyList(17)
         list_id.adapter = ExampleAdapter(exampleList, context as AppCompatActivity)
         list_id.layoutManager = LinearLayoutManager(context)
-
         list_id.setHasFixedSize(true)
+
+        var flag = true
+        btnSearch.setOnClickListener {
+            if (flag) {
+                edtSearch.visibility = Group.VISIBLE
+                flag = !flag
+            } else if (edtSearch.text.equals("")) {
+                edtSearch.visibility = Group.INVISIBLE
+                flag = !flag
+            }
+        }
     }
 
     private fun generateDummyList(size: Int): List<ExampleItem> {
